9d8c958dbe1f162647fcc3edbe4dd9554473d7d8
Alexey <gasfaksmgfsamhfas@gmail.com>
2025-09-19 00:54:21 +0300

    Пример простейшей формы

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace kursovaya.korviakov
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    private DataAccess _da;
+    private DataSet _projectsDs;
+    private BindingSource _projectsBs;
+
+    public Form1()
     {
-        public Form1()
+        InitializeComponent();
+        _da = new DataAccess();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        LoadProjects();
+    }
+
+    private void LoadProjects()
+    {
+        _projectsDs = _da.GetAllProjectsDataSet();
+        _projectsBs = new BindingSource { DataSource = _projectsDs.Tables["projects"] };
+        dgvProjects.DataSource = _projectsBs;
+    }
+
+    private void btnSaveProjects_Click(object sender, EventArgs e)
+    {
+        try
         {
-            InitializeComponent();
+            _da.UpdateProjectsFromDataSet(_projectsDs);
+            MessageBox.Show("Projects saved.");
+            LoadProjects();
         }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Error saving: " + ex.Message);
+        }
+    }
+
+    private void btnLoadTasks_Click(object sender, EventArgs e)
+    {
+        if (dgvProjects.CurrentRow == null) return;
+        var row = ((DataRowView)dgvProjects.CurrentRow.DataBoundItem).Row;
+        int projectId = Convert.ToInt32(row["id"]);
+        var dsTasks = _da.GetTasksByProjectDataSet(projectId);
+        dgvTasks.DataSource = dsTasks.Tables["tasks"];
+    }
+
+    private void btnRunSqlReport_Click(object sender, EventArgs e)
+    {
+        var dt = _da.GetProjectsByDeadline(dtpFrom.Value.Date, dtpTo.Value.Date);
+        dgvReportSql.DataSource = dt;
+    }
+
+    private void btnRunSp_Click(object sender, EventArgs e)
+    {
+        if (dgvProjects.CurrentRow == null) return;
+        var row = ((DataRowView)dgvProjects.CurrentRow.DataBoundItem).Row;
+        int projectId = Convert.ToInt32(row["id"]);
+        var dt = _da.GetProjectSummary(projectId);
+        dgvReportSp.DataSource = dt;
     }
 }
